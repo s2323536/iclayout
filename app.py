325a3129d62367e6e67db9778c34255357bb6b3c
smith <103059412+s2323536@users.noreply.github.com>
2023-03-09 01:17:21 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,request,render_template,jsonify,session
+import pandas as pd
+
+app = Flask(
+    __name__,
+    static_folder='static',
+    static_url_path='/'
+)
+app.secret_key="any string but secret"
+
+@app.route('/')
+def home():
+    return render_template('iclayout.html')
+
+        return render_template('iclayout.html', myResult="")
+    
